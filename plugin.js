9e2cb835fe4c416d0f2cc9c3424f14b3a23bd057
Lew <lewisyubu@gmail.com>
2018-09-17 12:02:12 -0500

    testing integration with nuxt-i18n

--- plugin.js
+++ plugin.js
@@ -4,5 +4,25 @@ import VeeValidate, { Validator } from 'vee-validate'
 import <%= options.lang %> from 'vee-validate/dist/locale/<%= options.lang %>'
 
 Validator.localize('<%= options.lang %>', <%= options.lang %>)
-<% } %>
 Vue.use(VeeValidate, <%= JSON.stringify(options.nuxtValidateOptions, null, 2) %>)
+<% } %>
+
+
+<% if (options.nuxti18n) { %>
+  export default function ({ app }) {
+    const lang = app.i18n.loadedLanguages[0]
+    import <%= lang %> from 'vee-validate/dist/locale/<%= lang %>'
+
+    //Loading languages for Vee
+    Validator.localize('<%= lang %>', <%= lang %>)
+
+    //Localizing the app when user refresh or access a localized link
+    Validator.localize((lang));
+
+    //Called everytime language change
+    app.i18n.beforeLanguageSwitch = (oldLocale, newLocale) => {
+      Validator.localize(newLocale);
+    }
+  }
+<% } %>
+
